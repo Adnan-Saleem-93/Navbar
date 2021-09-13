cff1244fdb195402c22eb63dfbf9d04f593b93da
Adnan Saleem <adnan13893@gmail.com>
2021-09-13 13:08:03 +0500

    initial layout setup with text, icons and css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from "react";
 import "./App.css";
+import {SocialMedia, NavLinks} from "./constants";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <section className="navbar">
+      <h1 className="home">
+        <a href="/">Nav App</a>
+      </h1>
+      <article className="nav-links">
+        {NavLinks.map((link, index) => {
+          let {id, url, text} = link;
+          return (
+            <a className="nav-item" key={index} href={url} id={`link-${id}`}>
+              {text}
+            </a>
+          );
+        })}
+      </article>
+      <article className="social-media">
+        {SocialMedia.map((link, index) => {
+          let {id, url, icon, title} = link;
+          return (
+            <a
+              href={url}
+              title={title}
+              key={index}
+              id={`social-link-${id}`}
+              className="social-link"
+            >
+              {icon}
+            </a>
+          );
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
