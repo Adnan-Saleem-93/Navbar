ad50ca099b7aa3d0fde4ae0b9ff637b46d17af17
Adnan Saleem <adnan13893@gmail.com>
2021-09-14 17:55:47 +0500

    used useRef to adjust nav height and nav toggle

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
-import React, {useState} from "react";
-import "./App.css";
-import {SocialMedia, NavLinks} from "./constants";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBars} from "@fortawesome/free-solid-svg-icons";
+import React, {useState, useEffect, useRef} from 'react'
+import './App.css'
+import {SocialMedia, NavLinks} from './constants'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
-  const [showLinks, setShowLinks] = useState(false);
+  const [showLinks, setShowLinks] = useState(false)
+  const navRef = useRef(null)
+  const navAreaRef = useRef(null)
+  const toggleRef = useRef(null)
+
+  useEffect(() => {
+    let navLinkHeight = navRef.current.getBoundingClientRect().height
+    if (showLinks) {
+      toggleRef.current.classList.add('rotate-toggle')
+      navAreaRef.current.style.height = `${navLinkHeight}px`
+    } else {
+      toggleRef.current.classList.remove('rotate-toggle')
+      navAreaRef.current.style.height = '0px'
+    }
+  }, [showLinks])
 
   return (
     <section className="navbar">
-      <h1 className="home">
-        <a href="/">NavApp</a>
-      </h1>
-      <article className="nav-links">
-        {NavLinks.map((link, index) => {
-          let {id, url, text} = link;
-          return (
-            <a className="nav-item" key={index} href={url} id={`link-${id}`}>
-              {text}
-            </a>
-          );
-        })}
+      <article className="nav-header">
+        <h1 className="home">
+          <a href="/">NavApp</a>
+        </h1>
+        <FontAwesomeIcon
+          icon={faBars}
+          className="nav-toggle"
+          onClick={() => setShowLinks(!showLinks)}
+          forwardedRef={toggleRef}
+        />
+      </article>
+      <article className="links-area" ref={navAreaRef}>
+        <ul className="nav-links" ref={navRef}>
+          {NavLinks.map((link, index) => {
+            let {id, url, text} = link
+            return (
+              <li key={index}>
+                <a className="nav-item" href={url} id={`link-${id}`}>
+                  {text}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
       </article>
       <article className="social-media">
         {SocialMedia.map((link, index) => {
-          let {id, url, icon, title} = link;
+          let {id, url, icon, title} = link
           return (
             <a
               href={url}
@@ -37,16 +63,11 @@ function App() {
             >
               {icon}
             </a>
-          );
+          )
         })}
       </article>
-      <FontAwesomeIcon
-        icon={faBars}
-        className="nav-toggle"
-        onClick={() => setShowLinks(!showLinks)}
-      />
     </section>
-  );
+  )
 }
 
-export default App;
+export default App
